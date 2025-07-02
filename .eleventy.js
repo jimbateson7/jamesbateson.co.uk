21a0de5d5370369f12969828eabd3b43137a5e47
James Bateson <jim.bateson@outlook.com>
2025-07-02 14:36:38 +0100

    feat: trying to add custom language to syntax highlighting (nunjucks)

--- .eleventy.js
+++ .eleventy.js
@@ -18,6 +18,55 @@ const parseTransform = require('./src/transforms/parse-transform.js');
 const site = require('./src/_data/site.json');
 
 module.exports = (config) => {
+	// Plugins
+	config.addPlugin(eleventyImageTransformPlugin);
+	config.addPlugin(rssPlugin);
+	config.addPlugin(syntaxHighlight, {
+		alwaysWrapLineHighlights: true,
+		errorOnInvalidLanguage: false,
+		init: function({
+			Prism
+		}) {
+			Prism.languages['nunjucks'] = {
+				comment: /^\{#[\s\S]*?#\}/,
+				delimiter: {
+					pattern: /^\{(?:\{\{|[%\{])-?|-?(?:\}\}|[%\}])\}$/,
+					alias: 'punctuation',
+				},
+				string: {
+					pattern: /"[^"]*"|'[^']*'/,
+					greedy: true,
+				},
+				keyword: /\b(?:as|assign|break|(?:end)?(?:capture|case|comment|for|form|if|paginate|raw|style|tablerow|unless)|continue|cycle|decrement|echo|else|elsif|in|include|increment|limit|liquid|offset|range|render|reversed|section|when|with)\b/,
+				object: /\b(?:address|all_country_option_tags|article|block|blog|cart|checkout|collection|color|country|country_option_tags|currency|current_page|current_tags|customer|customer_address|date|discount_allocation|discount_application|external_video|filter|filter_value|font|forloop|fulfillment|generic_file|gift_card|group|handle|image|line_item|link|linklist|localization|location|measurement|media|metafield|model|model_source|order|page|page_description|page_image|page_title|part|policy|product|product_option|recommendations|request|robots|routes|rule|script|search|selling_plan|selling_plan_allocation|selling_plan_group|shipping_method|shop|shop_locale|sitemap|store_availability|tax_line|template|theme|transaction|unit_price_measurement|user_agent|variant|video|video_source)\b/,
+				function: [{
+						pattern: /(\|\s*)\w+/,
+						lookbehind: true,
+						alias: 'filter',
+					},
+					{
+						// array functions
+						pattern: /(\.\s*)(?:first|last|size)/,
+						lookbehind: true,
+					},
+				],
+				boolean: /\b(?:false|nil|true)\b/,
+				range: {
+					pattern: /\.\./,
+					alias: 'operator',
+				},
+				// https://github.com/Shopify/liquid/blob/698f5e0d967423e013f6169d9111bd969bd78337/lib/liquid/lexer.rb#L21
+				number: /\b\d+(?:\.\d+)?\b/,
+				operator: /[!=]=|<>|[<>]=?|[|?:=-]|\b(?:and|contains(?=\s)|or)\b/,
+				punctuation: /[.,\[\]()]/,
+				empty: {
+					pattern: /\bempty\b/,
+					alias: 'keyword',
+				},
+			};
+		},
+	});
+
 	// Filters
 	config.addFilter('dateFilter', dateFilter);
 	config.addFilter('secondsFilter', secondsFilter);
@@ -40,8 +89,6 @@ module.exports = (config) => {
 	config.addPassthroughCopy('src/admin/config.yml');
 	config.addPassthroughCopy('src/admin/previews.js');
 	config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
-
-	config.addPlugin(eleventyImageTransformPlugin);
 
 	const now = new Date();
 
@@ -72,10 +119,6 @@ module.exports = (config) => {
 	config.addCollection('projects', (collection) => {
 		return [...collection.getFilteredByGlob('./src/project/*.md').filter(liveProject)].reverse();
 	});
-
-	// Plugins
-	config.addPlugin(rssPlugin);
-	config.addPlugin(syntaxHighlight);
 
 	// 404
 	config.setBrowserSyncConfig({
